dfcc268d4f6897ca450fd349583ee74f550111e2
IamGOOD <rukshenas1990@gmail.com>
2023-09-06 14:59:41 -0400

    added login.html

--- main/authentication/urls.py
+++ main/authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from .views import RegisterUser
 
 
 urlpatterns = [
+    path('registration/', RegisterUser.as_view(), name='registration'),
     path('login/', views.login, name='login'),
-    path('registration/', views.registration, name='registration'),
     path('logout/', views.logout, name='logout'),
 ]
--- main/authentication/views.py
+++ main/authentication/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 # data = {
 #     '1': 'login',
@@ -6,13 +10,21 @@ from django.shortcuts import render
 # }
 
 
+
 def login(requests):
     data = {'title': 'login'}
     return render(requests, 'main/login.html', context=data)
 
-def registration(requests):
-    data = {'title': 'registration'}
-    return render(requests, 'main.registration.html', context=data)
+# def registration(requests):
+#     data = {'title': 'registration'}
+#     return render(requests, 'main/register.html', context=data)
 
-def logout(requests):
-    pass
+# def logout_user(requests):
+#     logout(requests)
+#     return redirect('main/index.html')
+
+
+class RegisterUser(CreateView):
+    form_class = UserCreationForm
+    template_name = 'main/login.html'
+    success_url = reverse_lazy('login')
