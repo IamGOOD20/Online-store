627296dde4aaf440a7e491ca83913247b812e20a
IamGOOD <rukshenas1990@gmail.com>
2023-09-12 00:14:50 -0400

    Added register, login, logout

--- main/authentication/urls.py
+++ main/authentication/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-# from .views import RegisterUser
-
+from .views import RegisterUser, LoginUser
 
 urlpatterns = [
     # path('registration/', RegisterUser.as_view(), name='register'),
-    path('authentication/', views.login_user, name='authentication'),
+    path('login/', LoginUser.as_view(), name='login'),
     path('logout/', views.logout, name='logout'),
+    path('register/', RegisterUser.as_view(), name='register'),
 ]
--- main/authentication/views.py
+++ main/authentication/views.py
@@ -1,36 +1,29 @@
 from django.contrib.auth import logout, authenticate, login
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from . forms import LoginForm
+from .forms import RegisterUserForm
 
 
-def login_user(requests):
-    data = {'title': 'login', 'login_form': LoginForm()}
-    if requests.method == 'POST':
-        login_form = LoginForm(requests.POST)
-        if login_form.is_valid():
-            username = login_form.cleaned_data['username']
-            password = login_form.cleaned_data['password']
-            user = authenticate(username=username, password=password)
-            if user:
-                login(requests, user)
-                return redirect('index')
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'main/register.html'
+    success_url = reverse_lazy('login')
+    title = 'Register'
 
-    return render(requests, 'main/login.html', context=data)
 
-# def registration(requests):
-#     data = {'title': 'registration'}
-#     return render(requests, 'main/register.html', context=data)
+class LoginUser(LoginView):
+    form_class = AuthenticationForm
+    template_name = 'main/login.html'
+    title = 'Authorisation'
+
+    def get_success_url(self):
+        return reverse_lazy('index')
+
 
 def logout_user(requests):
     logout(requests)
     return redirect('index')
-
-
-# class RegisterUser(CreateView):
-#     form_class = UserCreationForm
-#     template_name = 'main/login.html'
-#     success_url = reverse_lazy('index')
 
